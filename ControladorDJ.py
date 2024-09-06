6206461b4975ec5d2c97f329983612014a67e638
lcanas_Software <socialmediaenterpises@gmail.com>
2024-09-06 00:06:24 -0300

    "ControladorDJ Version 2.0"

--- ControladorDJ.py
+++ ControladorDJ.py
@@ -1,30 +1,87 @@
 class Controlador_DJ:
         
-    def __init__(self,velocidad_play_der,velocidad_play_izq,canal_der,canal_izq):
-                 #,graves_vol_der,medios_vol_der,agudos_vol_der,graves_vol_izq,
-                 #medios_vol_izq,agudos_vol_izq,plato_der,plato_izq):
-        self.velocidad_play_der=velocidad_play_der
-        self.velocidad_play_izq=velocidad_play_izq
-        self.canal_der=canal_der
-        self.canal_izq=canal_izq
-        #self.__graves_vol_der=graves_vol_der
-        #self.__graves_vol_izq=graves_vol_izq
-        #self.__medios_vol_der=medios_vol_der
-        #self.__medios_vol_izq=medios_vol_izq
-        #self.__agudos_vol_der=agudos_vol_der
-        #self.__agudos_vol_izq=agudos_vol_izq
-        #self.__canal_der=canal_der
-        #self.__canal_izq=canal_izq
-        #self.__plato_der=plato_der
-        #self.__plato_izq=plato_izq
-    def cambiar_velocidad(self,canal,velocidad):
-        if canal=="Derecho":
-            velocidad=self.velocidad_play_der+velocidad
-            self.velocidad_play_der=velocidad
-            return "Velocidad Canal Derecho "+str(velocidad)
-        else:
-            velocidad=self.velocidad_play_izq+velocidad
-            self.velocidad_play_izq=velocidad
-            return "Velocidad Canal Izquierdo "+str(velocidad)       
+    def __init__(self,velocidad_play_canal_1,velocidad_play_canal_2,salida_canal_1,salida_canal_2,
+                 graves_vol_canal_1,medios_vol_canal_1,agudos_vol_canal_1,
+                 graves_vol_canal_2,medios_vol_canal_2,agudos_vol_canal_2,
+                 canal_1,canal_2):
+        self.velocidad_play_canal_1=velocidad_play_canal_1
+        self.velocidad_play_canal_2=velocidad_play_canal_2
+        self.salida_canal_der=salida_canal_1
+        self.salida_canal_izq=salida_canal_2
+        self.graves_vol_canal_1=graves_vol_canal_1
+        self.graves_vol_canal_2=graves_vol_canal_2
+        self.medios_vol_canal_1=medios_vol_canal_1
+        self.medios_vol_canal_2=medios_vol_canal_2
+        self.agudos_vol_canal_1=agudos_vol_canal_1
+        self.agudos_vol_canal_2=agudos_vol_canal_2
+        self.canal_1=canal_1
+        self.canal_2=canal_2
+#Metodo para cambiar la velocidad de reproduccion, 
+#Lo pense como un plato en el que puedo definir con un boton primero que canal y luego la velocidad a cambiar
+#segun gire mas el plato o no. 
     
+    def cambiar_velocidad(self,canal,velocidad):
+        if canal=="Canal 1":
+            velocidad=self.velocidad_play_canal_1+velocidad
+            self.velocidad_play_canal_1=velocidad
+            return "Velocidad Canal 1: "+str(velocidad)
+        else:
+            velocidad=self.velocidad_play_canal_2+velocidad
+            self.velocidad_play_canal_2=velocidad
+            return "Velocidad Canal 2: "+str(velocidad)
+
+#Metodo para subir o bajar el volumen de graves, medios o agudos,
+#Lo simplifique con un boton en el que elijo Canal 1, otro en el que elijo que tipo de sonido y 
+#la perilla que me da como parametro el aumento o disminucion de potencia.
+#En un controlador normal cada tipo de sonido, en cada canal, tendria su propia perilla       
+    def volumen(self,canal,sonido,potencia):
+        if canal=="Canal 1":
+            if sonido=="Graves":
+                potencia=self.graves_vol_canal_1+potencia
+                self.graves_vol_canal_1=potencia
+                return "Volumen Graves Canal 1: "+str(potencia)
+            elif sonido=="Medios":
+                potencia=self.medios_vol_canal_1+potencia
+                self.medios_vol_canal_1=potencia
+                return "Volumen Medios Canal 1: "+str(potencia)
+            elif sonido=="Agudos":
+                potencia=self.agudos_vol_canal_1+potencia
+                self.agudos_vol_canal_1=potencia
+                return "Volumen Agudos Canal 1: "+str(potencia)   
+        else:
+            if sonido=="Graves":
+                potencia=self.graves_vol_canal_2+potencia
+                self.graves_vol_canal_2=potencia
+                return "Volumen Graves Canal 2: "+str(potencia)
+            elif sonido=="Medios":
+                potencia=self.medios_vol_canal_2+potencia
+                self.medios_vol_canal_2=potencia
+                return "Volumen Medios Canal 2: "+str(potencia)
+            elif sonido=="Agudos":
+                potencia=self.agudos_vol_canal_2+potencia
+                self.agudos_vol_canal_2=potencia
+                return "Volumen Agudos Canal 2: "+str(potencia)          
+#En este metodo defino el Mix de salida de los canales al PARLANTE.
+    def cambiar_salida(self,crossfader):
+        if crossfader==0:
+            self.canal_1="Parlante"
+            self.canal_2="Parlante"
+            return "Canal 1 y Canal 2 en Parlante al 100%"
+        elif crossfader==1:
+            self.canal_1="Auricular"
+            self.canal_2="Parlante"
+            return "Canal 1 en Auricular al 100% y Canal 2 en Parlante al 100%"
+        elif crossfader==-1:
+            self.canal_1="Parlante"
+            self.canal_2="Auricular"
+            return "Canal 1 en Parlante al 100% y Canal 2 en Auricular al 100%"
+        elif crossfader<0:
+            self.canal_1="Parlante en"+str(int((abs(crossfader)*100)))+"%"
+            self.canal_2="Parlante en"+str(int(((1+crossfader)*100)))+"%"
+            return "Canal 1 en Parlante al "+str(int(abs(crossfader)*100))+"% y Canal 2 en Parlante al "+str(int((1+crossfader)*100))+"%"
+        else:
+            self.canal_2="Parlante en"+str(int(abs(crossfader)*100))+"%"
+            self.canal_1="Parlante en"+str(int((1-crossfader)*100))+"%"
+            return "Canal 1 en Parlante al "+str(int((1-crossfader)*100))+"% y Canal 2 en Parlante al "+str(int(crossfader*100))+"%"
+        
 
--- test_ControladorDJ.py
+++ test_ControladorDJ.py
@@ -1,8 +1,19 @@
 from ControladorDJ import Controlador_DJ
 
 def test_ControladorDJ():
-    pionnerDJ=Controlador_DJ(1,1,"Auricular","Parlante")
-    assert pionnerDJ.cambiar_velocidad("Derecho",0.5) == "Velocidad Canal Derecho 1.5"
-    assert pionnerDJ.cambiar_velocidad("Izquierdo",0.5) == "Velocidad Canal Izquierdo 1.5"
-    assert pionnerDJ.cambiar_velocidad("Derecho",0.5) == "Velocidad Canal Derecho 2.0"
-    assert pionnerDJ.cambiar_velocidad("Izquierdo",0.5) == "Velocidad Canal Izquierdo 2.0"
+    pionnerDJ=Controlador_DJ(1,1,"Auricular","Parlante",0,0,0,0,0,0,"Salida por Parlante","Salida por Auricular")
+    assert pionnerDJ.cambiar_velocidad("Canal 1",0.5) == "Velocidad Canal 1: 1.5"
+    assert pionnerDJ.cambiar_velocidad("Canal 2",0.5) == "Velocidad Canal 2: 1.5"
+    assert pionnerDJ.cambiar_velocidad("Canal 1",0.5) == "Velocidad Canal 1: 2.0"
+    assert pionnerDJ.cambiar_velocidad("Canal 2",0.5) == "Velocidad Canal 2: 2.0"
+    assert pionnerDJ.volumen("Canal 1","Agudos",0.25) == "Volumen Agudos Canal 1: 0.25"
+    assert pionnerDJ.volumen("Canal 1","Agudos",0.25) == "Volumen Agudos Canal 1: 0.5"
+    assert pionnerDJ.volumen("Canal 1","Graves",0.25) == "Volumen Graves Canal 1: 0.25"
+    assert pionnerDJ.volumen("Canal 2","Graves",0.25) == "Volumen Graves Canal 2: 0.25"
+    assert pionnerDJ.volumen("Canal 1","Medios",0.25) == "Volumen Medios Canal 1: 0.25"
+    assert pionnerDJ.volumen("Canal 1","Medios",0.25) == "Volumen Medios Canal 1: 0.5"
+    assert pionnerDJ.cambiar_salida(-0.3)=="Canal 1 en Parlante al 30% y Canal 2 en Parlante al 70%"
+    assert pionnerDJ.cambiar_salida(-1)=="Canal 1 en Parlante al 100% y Canal 2 en Auricular al 100%"
+    assert pionnerDJ.cambiar_salida(1)=="Canal 1 en Auricular al 100% y Canal 2 en Parlante al 100%"
+    assert pionnerDJ.cambiar_salida(0.4)=="Canal 1 en Parlante al 60% y Canal 2 en Parlante al 40%"
+    assert pionnerDJ.cambiar_salida(0)=="Canal 1 y Canal 2 en Parlante al 100%"
